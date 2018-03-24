45df1777a666211a4a35b1341568009ed756f06e
abdullah <jello.4488@gmail.com>
2018-03-24 04:20:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var rect = {
+    parimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b){
+    console.log(" l= "+l+" b= "+b);
+    
+    if (l <= 0 || b <=0){
+        console.log("rect dimensions should be grater than zero");
+    }
+    else {
+        console.log("The area of the rect is "+rect.area(l,b));
+        console.log("The parimeter of the rect is "+rect.parimeter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(0,5);
